cff85c71b1d0ca0afde836f43007020fccc66695
harshithi5 <harshitkumarsingh2609@gmail.com>
2025-05-31 07:26:58 +0530

    Update app.js
    uploads->Uploads

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const PORT = 3000;
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/'); // save files in 'uploads' folder
+    cb(null, 'Uploads/'); // save files in 'uploads' folder
   },
   filename: function (req, file, cb) {
     // save files with a unique name: current timestamp + original extension
